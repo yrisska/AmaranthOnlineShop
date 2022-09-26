2459c8cb8516e7b35fa24df51f69eabe95580171
Ярослав Антонов <yriss2003@gmail.com>
2022-09-27 00:21:11 +0300

    added exception handling and db transaction middleware to api pipeline, small fixes

---
+++ AmaranthOnlineShop.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using AmaranthOnlineShop.API.Middlewares;
+
+namespace AmaranthOnlineShop.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        public static IApplicationBuilder UseDbTransaction(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<DbTransactionMiddleware>();
+    }
+}
---
+++ AmaranthOnlineShop.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using AmaranthOnlineShop.API.Middlewares.Models;
+using FluentValidation;
+using System.Net;
+
+namespace AmaranthOnlineShop.API.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception e)
+            {
+                httpContext.Response.StatusCode = e switch
+                {
+                    ValidationException _ => (int)HttpStatusCode.BadRequest,
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+
+                httpContext.Response.ContentType = "application/json";
+
+                await httpContext.Response.WriteAsync(new ErrorDetails()
+                {
+                    StatusCode = httpContext.Response.StatusCode,
+                    Message = e.Message
+                }.ToString());
+            }
+        }
+    }
+}
